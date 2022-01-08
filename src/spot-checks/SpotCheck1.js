2b4074aa28c22d2002325de3808b1b043fa3bc53
faroky4 <faroky4@gmail.com>
2022-01-08 23:44:51 +0200

    solved the spot checks

--- src/spot-checks/SpotCheck1.js
+++ src/spot-checks/SpotCheck1.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 export default function SpotCheck1() {
-  // your code here
+  const [cuurNumber , setNumber] = useState(0);
+
+  const increas = ()=> setNumber(cuurNumber +1)
+  const dencreas = ()=> setNumber(cuurNumber -1) 
   
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 1</h4>
       <div className="exercise" id="sc-1">
-        {/* your code here... */}
+        <h2>{cuurNumber}</h2>
+        <button onClick={increas}> increas</button>
+        <button onClick={dencreas}> dencreas</button>
 
       </div>
     </div>
